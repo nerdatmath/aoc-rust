69e2e6b91804922d56391d7e1d695dc2edbe8d42
Matt Harden <matt.harden@gmail.com>
2025-07-02 19:03:12 -0700

    2023 day 7.

---
+++ 2023/07/src/part2/mod.rs
@@ -0,0 +1,17 @@
+use crate::{card::HandWithJokers, puzzle::Puzzle};
+
+pub fn run(input: &str) -> usize {
+    let puzzle: Puzzle<HandWithJokers> = input.parse().expect("parse failed");
+    puzzle.winnings()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::data::EXAMPLE1;
+
+    #[test]
+    fn test1() {
+        assert_eq!(run(EXAMPLE1), 5905);
+    }
+}
